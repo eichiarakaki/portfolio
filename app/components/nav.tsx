7867da4922b2b1ce10dabb26863e8b24b77ce43d
Eichi Arakaki <justanunusualperson@gmail.com>
2025-12-13 23:32:30 +0900

    fixing bad page endpoint

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -34,7 +34,7 @@ export const Navigation: React.FC = () => {
 							Projects
 						</Link>
 						<Link
-							href="/about-me"
+							href="/about"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
 							About
